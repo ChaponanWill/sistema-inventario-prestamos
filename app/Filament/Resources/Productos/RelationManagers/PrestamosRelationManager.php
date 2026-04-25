5b24c2f0c07b43b4eafb2cd56e6760ab6e4fa0f9
ChaponanWill <chapowilliam09@gmail.com>
2026-04-25 09:59:52 -0500

    Se creó correctamente los enlaces a préstamo.

--- app/Filament/Resources/Productos/RelationManagers/PrestamosRelationManager.php
+++ app/Filament/Resources/Productos/RelationManagers/PrestamosRelationManager.php
@@ -2,128 +2,22 @@
 
 namespace App\Filament\Resources\Productos\RelationManagers;
 
-use Filament\Actions\AssociateAction;
-use Filament\Actions\BulkActionGroup;
+use App\Filament\Resources\Prestamos\PrestamoResource;
 use Filament\Actions\CreateAction;
-use Filament\Actions\DeleteAction;
-use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\DissociateAction;
-use Filament\Actions\DissociateBulkAction;
-use Filament\Actions\EditAction;
-use Filament\Actions\ViewAction;
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
-use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\RelationManagers\RelationManager;
-use Filament\Schemas\Schema;
-use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
 
 class PrestamosRelationManager extends RelationManager
 {
     protected static string $relationship = 'prestamos';
 
-    public function form(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-                DatePicker::make('fecha_prestamo')
-                    ->required(),
-                TextInput::make('cantidad')
-                    ->rules(['integer', 'min:1'])
-                    // No dejar insertar números superiores al stock disponible
-                    ->maxValue(fn ($component) => $component->getContainer()->getLivewire()->getOwnerRecord()->cantidad)
-                    ->required()
-                    ->numeric(),
-                DatePicker::make('fecha_devolucion'),
-                Select::make('prestamista_id')
-                    ->relationship('prestamista', 'dni', fn(Builder $query, $get) => $query->where('estado', 1)->orWhere('id', $get('prestamista_id')))
-                    ->searchable()
-                    ->getOptionLabelFromRecordUsing(
-                        fn($record) =>
-                        $record->dni . ' - ' . $record->primer_nombre . ' ' . $record->primer_apellido
-                    )
-                    ->required(),
-                Textarea::make('descripcion')
-                    ->default(null)
-                    ->columnSpanFull(),
-            ]);
-    }
-
-    public function infolist(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-                TextEntry::make('fecha_prestamo')
-                    ->date(),
-                TextEntry::make('cantidad')
-                    ->numeric(),
-                TextEntry::make('fecha_devolucion')
-                    ->date()
-                    ->placeholder('-'),
-                TextEntry::make('prestamista.dni')
-                    ->getStateUsing(function ($record) {
-                        return $record->prestamista->dni . ' - ' . $record->prestamista->primer_nombre . ' ' . $record->prestamista->primer_apellido;
-                    })
-                    ->label('Prestamista'),
-                TextEntry::make('descripcion')
-                    ->label('Descripción')
-                    ->placeholder('-')
-                    ->columnSpanFull(),
-
-
-            ]);
-    }
+    protected static ?string $relatedResource = PrestamoResource::class;
 
     public function table(Table $table): Table
     {
         return $table
-            ->recordTitleAttribute('nombre')
-            ->columns([
-                TextColumn::make('fecha_prestamo')
-                    ->date()
-                    ->sortable(),
-                TextColumn::make('cantidad')
-                    ->numeric()
-                    ->sortable(),
-                TextColumn::make('fecha_devolucion')
-                    ->date()
-                    ->sortable(),
-                TextColumn::make('prestamista.dni')
-                    ->getStateUsing(function ($record) {
-                        return $record->prestamista->dni . ' - ' . $record->prestamista->primer_nombre . ' ' . $record->prestamista->primer_apellido;
-                    })
-                    ->searchable(),
-                TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-            ])
-            ->filters([
-                //
-            ])
             ->headerActions([
                 CreateAction::make(),
-                // AssociateAction::make(),
-            ])
-            ->recordActions([
-                ViewAction::make(),
-                EditAction::make(),
-                // DissociateAction::make(),
-                DeleteAction::make(),
-            ])
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    // DissociateBulkAction::make(),
-                    DeleteBulkAction::make(),
-                ]),
             ]);
     }
 }
