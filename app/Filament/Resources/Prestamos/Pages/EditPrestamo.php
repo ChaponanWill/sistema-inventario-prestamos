99f8fdb0d0ab57a3ce9fc8b8a16aba09ba3f9b41
ChaponanWill <chapowilliam09@gmail.com>
2026-04-24 23:46:25 -0500

    Todo completo en Préstamos

--- app/Filament/Resources/Prestamos/Pages/EditPrestamo.php
+++ app/Filament/Resources/Prestamos/Pages/EditPrestamo.php
@@ -10,12 +10,69 @@ use Filament\Resources\Pages\EditRecord;
 class EditPrestamo extends EditRecord
 {
     protected static string $resource = PrestamoResource::class;
+    // -----
+    // guardar valores originales
+    protected $cantidadOriginal;
+    protected $productoOriginal;
+
+    // antes de guardar
+    protected function beforeSave(): void
+    {
+        $this->cantidadOriginal = $this->record->cantidad;
+        $this->productoOriginal = $this->record->producto_id;
+    }
+
+    // después de guardar
+    protected function afterSave(): void
+    {
+        $prestamo = $this->record;
+
+        $original = $this->cantidadOriginal;
+        $nuevo = $prestamo->cantidad;
+
+        //  mismo producto
+        if ($this->productoOriginal == $prestamo->producto_id) {
+
+            if ($prestamo->producto) {
+                // devolver lo anterior
+                $prestamo->producto->increment('cantidad', $original);
+
+                // restar lo nuevo
+                $prestamo->producto->decrement('cantidad', $nuevo);
+            }
+
+        } else {
+            //  cambió de producto
+
+            // devolver al producto anterior
+            $productoAnterior = \App\Models\Producto::find($this->productoOriginal);
+            if ($productoAnterior) {
+                $productoAnterior->increment('cantidad', $original);
+            }
+
+            // restar al nuevo producto
+            if ($prestamo->producto) {
+                $prestamo->producto->decrement('cantidad', $nuevo);
+            }
+        }
+    }
+
+
+    // ---
 
     protected function getHeaderActions(): array
     {
         return [
             ViewAction::make(),
-            DeleteAction::make(),
+             DeleteAction::make()
+                ->after(function ($record) {
+
+                    if ($record->producto) {
+                        // devolver stock
+                        $record->producto->increment('cantidad', $record->cantidad);
+                    }
+
+                }),
         ];
     }
 }
