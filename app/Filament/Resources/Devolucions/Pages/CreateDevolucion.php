1c638aace0b5ed3bdea0db58aed059c7ff4d1756
ChaponanWill <chapowilliam09@gmail.com>
2026-04-26 22:41:34 -0500

    restamos al prestamo y aumentamos al producto. (Devoluciones)

--- app/Filament/Resources/Devolucions/Pages/CreateDevolucion.php
+++ app/Filament/Resources/Devolucions/Pages/CreateDevolucion.php
@@ -8,4 +8,26 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateDevolucion extends CreateRecord
 {
     protected static string $resource = DevolucionResource::class;
+
+    protected function afterCreate(): void
+    {
+        $devolucion = $this->record;
+
+        if ($devolucion->prestamo) {
+
+            //restar del préstamo (no recomendado, pero lo estás usando)
+            $devolucion->prestamo->decrement(
+                'cantidad',
+                $devolucion->cantidad_devuelta
+            );
+
+            // 🔺 sumar al producto
+            if ($devolucion->prestamo->producto) {
+                $devolucion->prestamo->producto->increment(
+                    'cantidad',
+                    $devolucion->cantidad_devuelta
+                );
+            }
+        }
+    }
 }
